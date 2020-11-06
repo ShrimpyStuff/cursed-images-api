350d9fe2839a1d56e8e63d6408fef27d447f4f5f
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-06 14:59:41 -0500

    search route (/api/search)

---
+++ src/api.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import fs from 'fs';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.status(200).json({
+		status: 200,
+		message: 'Some API message or idk',
+	});
+});
+
+router.get('/search', (req, res) => {
+	const images = fs.readdirSync('./public/images');
+
+	res.status(200).json({
+		status: 200,
+		url: `${req.protocol}://${req.get('host')}/images/${images[
+			Math.floor(Math.random() * images.length)
+		].replace(/\\/g, '/')}`,
+	});
+});
+
+export default router;
