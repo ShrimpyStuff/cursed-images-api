a3e812b781162f1293e376eda11c70fdfd83c1e0
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-11-08 15:31:06 -0500

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	res.status(200).json({
 		status: 200,
-		message: 'Go to <a href="/searcg">/search</a>',
+		message: 'Go to /search',
 	});
 });
 
